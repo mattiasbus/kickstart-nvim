38b8659ff5ad7c75d7b6134a70c09b2bd51c3319
Mattias Busck <mattias.busck@hostmobility.com>
2023-08-20 09:48:52 +0200

    init.lua: (personal) vim.keymap.set('n', ';', ':') and ('i','jk','<Esc>)

--- init.lua
+++ init.lua
@@ -325,6 +325,8 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set('n', ';', ':')
+vim.keymap.set('i', 'jk', '<Esc>')
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
